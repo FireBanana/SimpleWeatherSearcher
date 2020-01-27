4793f703478db7437a4036e5fe96ebde9427a2aa
Fire Banana <free0run@gmail.com>
2020-01-27 20:19:05 +0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -23,84 +23,122 @@ class MainBody extends StatefulWidget {
 }
 
 class MainBodyState extends State {
+  var popupItems = List<Widget>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GetMainAppBar(context),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: Theme.of(context).accentColor,
-            height: 270,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                child: Text(
-                  "30°C",
-                  style: mainTextStyle,
+        appBar: GetMainAppBar(context),
+        body: Column(
+          children: <Widget>[
+            Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Container(
+                  color: Theme.of(context).accentColor,
+                  height: 270,
                 ),
-                padding: EdgeInsets.only(left: 20, top: 20, bottom: 10),
-              ),
-              Padding(
-                child: Divider(
-                  color: Colors.white,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      child: Text(
+                        "30°C",
+                        style: mainTextStyle,
+                      ),
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 10),
+                    ),
+                    Padding(
+                      child: Divider(
+                        color: Colors.white,
+                      ),
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                    ),
+                    Padding(
+                      child: Text(
+                        "Precipitation: 10%",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 5),
+                    ),
+                    Padding(
+                      child: Text(
+                        "Mostly Sunny",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      padding: EdgeInsets.only(left: 20, bottom: 20),
+                    ),
+                    Padding(
+                      child: TextField(
+                        onSubmitted: (text) {
+                          GetData(text).then((data) => {
+                                this.setState(() {
+                                  var newItems = List<Widget>();
+
+                                  for (var item in ParseJSON(data).take(5)) {
+                                    newItems.add(ListTile(
+                                      leading: Icon(
+                                        Icons.location_city,
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                      title: Text(item,
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .accentColor)),
+                                      onTap: () {
+                                        debugPrint('tapped');
+                                      },
+                                    ));
+                                    popupItems = newItems;
+                                    debugPrint("got $item");
+                                  }
+                                })
+                              });
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(400),
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(400),
+                              borderSide: BorderSide(color: Colors.white)),
+                          hintText: "Enter city name...",
+                          hintStyle: TextStyle(color: Colors.white),
+                          labelText: "City",
+                          labelStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                    ),
+                  ],
                 ),
-                padding: EdgeInsets.only(left: 20, right: 20),
-              ),
-              Padding(
-                child: Text(
-                  "Precipitation: 10%",
-                  style: TextStyle(color: Colors.white),
-                ),
-                padding: EdgeInsets.only(left: 20, top: 20, bottom: 5),
-              ),
-              Padding(
-                child: Text(
-                  "Mostly Sunny",
-                  style: TextStyle(color: Colors.white),
-                ),
-                padding: EdgeInsets.only(left: 20, bottom: 20),
-              ),
-              Padding(//
-                child: TextField(
-                  onChanged: (text){
-                    GetData(text).then((data) => {ParseJSON(data)});
-                  },
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(400),
-                            borderSide: BorderSide(color: Colors.white)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(400),
-                            borderSide: BorderSide(color: Colors.white)),
-                        hintText: "Enter city name...",
-                        hintStyle: TextStyle(color: Colors.white),
-                        labelText: "City",
-                        labelStyle: TextStyle(color: Colors.white))),
-                padding: EdgeInsets.only(left: 20, right: 20),
-              )
-            ],
-          ),
-          Positioned(
-            child: RaisedButton(
-              child: Text(
-                'Refresh',
-                style: TextStyle(color: Colors.blue),
-              ),
-              color: Theme.of(context).cardColor,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-              onPressed: () {},
+                Positioned(
+                  child: RaisedButton(
+                    child: Text(
+                      'Search',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    color: Theme.of(context).cardColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: () {},
+                  ),
+                  top: 245,
+                  left: 100,
+                  right: 100,
+                )
+              ],
             ),
-            top: 245,
-            left: 100,
-            right: 100,
-          )
-        ],
-      ),
-    );
+            Expanded(
+              child: ListView(
+                children: popupItems,
+                physics: NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.only(top: 25),
+              ),
+            )
+          ],
+        ));
   }
 }
 
